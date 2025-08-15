767f75c12cf30a171553ab0c6da5ec12dcd8109b
Raj Khochare <rajkhochare2005@gmail.com>
2025-08-15 22:40:02 +0530

    Initial commit - Rescue Guide app

--- app/src/main/java/com/example/rescuebuddy/MainActivity.kt
+++ app/src/main/java/com/example/rescuebuddy/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.rescuebuddy.ui.screens.RescueGuideApp
+import com.example.rescuebuddy.screens.RescueGuideApp
 import com.example.rescuebuddy.ui.theme.RescueBuddyTheme
 
 class MainActivity : ComponentActivity() {
