9fd95d109ab0049a0743f33733dca5d66eb31a64
Raj Khochare <rajkhochare2005@gmail.com>
2025-08-15 22:20:27 +0530

    Initial commit - Rescue Guide app

---
+++ app/src/main/java/com/example/rescuebuddy/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.rescuebuddy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.rescuebuddy.ui.screens.RescueGuideApp
+import com.example.rescuebuddy.ui.theme.RescueBuddyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RescueBuddyTheme {
+                RescueGuideApp()
+            }
+        }
+    }
+}
+
+
